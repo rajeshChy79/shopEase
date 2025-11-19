9112b7566f17d0df0401e582f3cf558ac5dbff48
Rajesh Kumar Choudhary <128174090+rajeshChy79@users.noreply.github.com>
2025-11-20 02:09:55 +0530

    Refactor cartSchema to use ObjectId and defaults

--- backend/models/cartProduct.js
+++ backend/models/cartProduct.js
@@ -1,15 +1,21 @@
 const mongoose=require("mongoose");
 
-const cartSchema=mongoose.Schema({
-     productId:{
-      ref:'product',
-      type:String
-     },
-     quantity:Number,
-     userId:String
-},{
-  timestamps:true
-})
+const cartSchema = mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true
+  },
+  quantity: {
+    type: Number,
+    default: 1
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
+}, {
+  timestamps: true
+});
 
-const cartModel=mongoose.model("addToCart",cartSchema)
-module.exports=cartModel
+module.exports = mongoose.model("addToCart", cartSchema);
