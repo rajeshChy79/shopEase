a58195397be02e1ad2e82a7f9f76c2a442bf7aa7
Rajesh Kumar Choudhary <128174090+rajeshChy79@users.noreply.github.com>
2025-12-26 16:36:55 +0530

    Refactor cartSchema definition in cartProduct.js

--- backend/models/cartProduct.js
+++ backend/models/cartProduct.js
@@ -1,29 +1,25 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const cartSchema = mongoose.Schema({
-  productId: {
-    type: mongoose.Schema.Types.ObjectId,
-<<<<<<< HEAD
-    ref: "Product",   // MUST match the model name exactly
-=======
-    ref: "Product",
->>>>>>> 9112b7566f17d0df0401e582f3cf558ac5dbff48
-    required: true
+const cartSchema = new mongoose.Schema(
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      default: 1,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  quantity: {
-    type: Number,
-    default: 1
-  },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-<<<<<<< HEAD
-    ref: "User"   // if you have a User model
-=======
-    ref: "User"
->>>>>>> 9112b7566f17d0df0401e582f3cf558ac5dbff48
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-});
+);
 
 module.exports = mongoose.model("addToCart", cartSchema);
