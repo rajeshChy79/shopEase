e3fdae56ba6457bc67179b6f2e54c8f452722885
Rajesh Kumar Choudhary <128174090+rajeshChy79@users.noreply.github.com>
2025-12-31 11:15:00 +0530

    Update cloudinary.js

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,10 +1,5 @@
 const cloudinary = require("cloudinary").v2;
 
-console.log("Configuring Cloudinary with env variables:", {
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET});
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
